999a72129c072fbd8d833d3e6cd95b0c45be3f20
Juan Carlos Castillo <86281300+CastilloJC@users.noreply.github.com>
2021-11-10 23:57:26 -0500

    Add files via upload

---
+++ src/components/carwidget/CarWidget.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CarWidget = () => {
+    return (
+        <div>
+            <i className="bi bi-cart"></i>
+        </div>
+    )
+}
+
+export default CarWidget
